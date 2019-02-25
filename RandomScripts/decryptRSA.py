ba3d22070b5720282e1e5911b4a861c6fdec5c8c
Hacker Man <hackerman@localhost.localdomain>
2019-02-25 18:25:37 +0100

    Initial commit

---
+++ RandomScripts/decryptRSA.py
@@ -0,0 +1,34 @@
+def egcd(a, b):
+    x,y, u,v = 0,1, 1,0
+    while a != 0:
+        q, r = b//a, b%a
+        m, n = x-u*q, y-v*q
+        b,a, x,y, u,v = a,r, u,v, m,n
+        gcd = b
+    return gcd, x, y
+
+def main():
+
+    p = 1090660992520643446103273789680343
+    q = 1162435056374824133712043309728653
+    e = 65537
+    ct = 299604539773691895576847697095098784338054746292313044353582078965
+
+    # compute n
+    n = p * q
+
+    # Compute phi(n)
+    phi = (p - 1) * (q - 1)
+
+    # Compute modular inverse of e
+    gcd, a, b = egcd(e, phi)
+    d = a
+
+    print( "n:  " + str(d) );
+
+    # Decrypt ciphertext
+    pt = pow(ct, d, n)
+    print( "pt: " + str(pt) )
+
+if __name__ == "__main__":
+    main()
